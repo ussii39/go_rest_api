337ac5056f7662e2bdef675f0c4e1ec1a24d87e5
ussii39 <usimarutetunori3@outlook.jp>
2023-01-29 16:43:39 +0900

    mysql関連

--- db/mysql.go
+++ db/mysql.go
@@ -45,11 +45,11 @@ func getConnectionString() string {
 	// 本番環境では127.0.0.1
 	host := getParamString("MYSQL_DB_HOST", "127.0.0.1")
 	port := getParamString("MYSQL_PORT", "3306")
-	user := getParamString("MYSQL_USER", "root")
+	user := getParamString("MYSQL_USER", "dbUser")
 	// local環境では
 	// pass := getParamString("MYSQL_PASSWORD", "")
 	// 本番環境では
-	pass := getParamString("MYSQL_PASSWORD", "password")
+	pass := getParamString("MYSQL_PASSWORD", "tetunori3")
 	dbname := getParamString("MYSQL_DB", "todoList")
 	protocol := os.Getenv("MYSQL_PROTOCOL")
 
